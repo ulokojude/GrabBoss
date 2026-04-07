a37b0c50885b47499cab4e2ecd89d7adf2af632f
ulokojude <myadonaitech@gmail.com>
2026-04-07 15:16:23 +0100

    ...

--- data.js
+++ data.js
@@ -1,12 +1,8 @@
 export const products = [
   {
+    "id" : "1231-234-4-43-34-3432",
     "name" : "jude",
-    "price" : "888",
-    "image" : "images/products/backbag.jpg",
-  },
-  {},
-  {},
-  {},
-  {},
-  {}
-]
+    "price" : 888,
+    "image" : "images/products/backbag.jpg"
+  }
+];
--- products.js
+++ products.js
@@ -1,18 +1,37 @@
-import { products } from "./data";
+import { products } from "data";
 
-const container = document.getElementById("main");
+const container = document.getElementById("js-products-grid");
 
 products.forEach(product => {
   container.innerHTML += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image" alt="" src="${product.image}">
-      </div>
-      <p>${product.price}</p> 
-      <div class="product-price">
+      </div> 
+      <div style="font-weight:bolder; color:#0d6efd" class="product-name limit-text-to-2-lines" style="color: #333;">
         ${product.name}
       </div>
-      <div class="product-spacer"></div>
-    </div>`;
+      <div class="product-rating-container">
+        <a href="#">
+          <img class="product-rating-stars"
+            src="${product}"
+          >
+        </a>
+        <div class="product-rating-count link-primary">
+          ${product.rating_count}
+        </div>
+      </div>
+
+      <p class=" pr">
+        &#8358; ${product.price}
+      </p>
+
+      <form>
+        <button class="btn btn-primary" name="add_to_cart" type="submit">
+          Add to Cart
+        </button>
+      </form>
+    </div>
+  `;
 });
 
